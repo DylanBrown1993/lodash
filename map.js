aea64c2e01a238395506e2486d1330622012ba57
= <=>
2023-05-06 16:02:52 -0400

    added map

--- map.js
+++ map.js
@@ -1,30 +1,37 @@
-/**
- * Creates an array of values by running each element of `array` thru `iteratee`.
- * The iteratee is invoked with three arguments: (value, index, array).
- *
- * @since 5.0.0
- * @category Array
- * @param {Array} array The array to iterate over.
- * @param {Function} iteratee The function invoked per iteration.
- * @returns {Array} Returns the new mapped array.
- * @example
- *
- * function square(n) {
- *   return n * n
- * }
- *
- * map([4, 8], square)
- * // => [16, 64]
- */
-function map(array, iteratee) {
-  let index = -1
-  const length = array == null ? 0 : array.length
-  const result = new Array(length)
+const words = ["ground", "control", "to", "major", "tom"];
 
-  while (++index < length) {
-    result[index] = iteratee(array[index], index, array)
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
-  return result
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-export default map
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
+  return results;
+}
+
+const expected = [6, 7, 2, 5, 3];
+const result = map(words, word => word.length);
+assertEqual(eqArrays(result, expected), true);
